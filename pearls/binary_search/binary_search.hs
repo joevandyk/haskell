ba397b625aff60cc17370f75b1b4022dc58efeeb
Joe Van Dyk <joe@fixieconsulting.com>
2010-01-01 12:01:07 -0800

    Using guards instead of crappy if statements

--- pearls/binary_search/binary_search.hs
+++ pearls/binary_search/binary_search.hs
@@ -1,21 +1,15 @@
 -- A translation of http://en.wikipedia.org/wiki/Binary_search_algorithm#Recursive
-binary_find :: Ord a => [a] -> a -> Maybe Int
-binary_find [] elem   = Nothing
-
 binary_find list elem = 
   do_search list elem 0 (length list - 1) 
   where
-    do_search list elem low high = 
-      if high < low then Nothing
-      else 
-        if list !! mid > elem
-        then do_search list elem low (mid - 1)
-        else
-          if list !! mid < elem
-          then do_search list elem (mid + 1) high 
-          else Just mid
+    do_search list elem low high
+      | high    < low   = Nothing
+      | mid_val > elem  = do_search list elem low     (mid-1)
+      | mid_val < elem  = do_search list elem (mid+1) high
+      | otherwise       = Just mid
       where
-        mid = low + (high - low) `div` 2
+        mid     = low + (high - low) `div` 2
+        mid_val = list !! mid
 
 main = do
   print $ binary_find [1] 1
@@ -23,12 +17,12 @@ main = do
   print $ binary_find [1,3,4] 3
   print $ binary_find [1,3,4] 4
   print $ binary_find [1,2,4,6,8,9,12,15,17,20] 17
+  print $ binary_find [1,2,4,6,8,9,12,15,17,20] 20
   print $ binary_find "hello" 'l'
   print $ binary_find [0.0, 1.5, 3.0] 3.0
 
   print $ binary_find [] 1
   print $ binary_find [1,3] 2
   print $ binary_find [1,4,6,8,9,12,15,17,20] 2
-
-  -- boom?
   print $ binary_find [1,4,6,8,9,12,15,17,20] 100
+  print $ binary_find [1,4,6,8,9,12,15,17,20] (-100)
