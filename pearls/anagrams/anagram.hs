aa61a58e6a027dda60a32ae64ec99f92d00ae5ed
Joe Van Dyk <joe@fixieconsulting.com>
2010-01-05 05:57:18 -0800

    First attempt at using bytestrings

--- pearls/anagrams/anagram.hs
+++ pearls/anagrams/anagram.hs
@@ -1,7 +1,10 @@
 import qualified Data.Map as Map
 import Data.Ord
 import Data.List
+import qualified Data.ByteString.Char8 as BS
 
+-- what's the type here?  I get an infinite type error
+anagrams :: Ord a => [a] -> [a]
 anagrams words = 
   sorted_anagrams
   where  
@@ -10,5 +13,9 @@ anagrams words =
     insert_word map word = Map.insertWith' (++) (sort word) [word] map
 
 main = do
-  input <- getContents
-  print $ take 3 $ anagrams $ lines input
+  -- original code, worked fine:
+  -- input <- getContents
+  -- print $ take 3 $ anagrams $ lines input
+  -- new code with bytestring has errors
+  input <- BS.getContents
+  print $ take 3 $ anagrams $ BS.lines input
