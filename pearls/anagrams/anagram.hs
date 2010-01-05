da7dbaf34bb1893ff03d2dd3cc4b8ccd6dbe98eb
Joe Van Dyk <joe@fixieconsulting.com>
2010-01-05 07:52:36 -0800

    More work with bytestrings

--- pearls/anagrams/anagram.hs
+++ pearls/anagrams/anagram.hs
@@ -3,19 +3,15 @@ import Data.Ord
 import Data.List
 import qualified Data.ByteString.Char8 as BS
 
--- what's the type here?  I get an infinite type error
-anagrams :: Ord a => [a] -> [a]
+anagrams :: [BS.ByteString] -> [[BS.ByteString]]
 anagrams words = 
   sorted_anagrams
   where  
     sorted_anagrams      = sortBy (flip $ comparing length) get_anagrams
     get_anagrams         = Map.elems $ foldl' insert_word Map.empty words
-    insert_word map word = Map.insertWith' (++) (sort word) [word] map
+    insert_word map word = Map.insertWith' (++) sorted_word [word] map
+    sorted_word word     = BS.pack . sort . BS.unpack $ word
 
 main = do
-  -- original code, worked fine:
-  -- input <- getContents
-  -- print $ take 3 $ anagrams $ lines input
-  -- new code with bytestring has errors
   input <- BS.getContents
   print $ take 3 $ anagrams $ BS.lines input
