f039da3172eeabcab9b7294d801f322450a0c526
Joe Van Dyk <joe@fixieconsulting.com>
2012-08-08 16:30:53 -0700

    started problems

---
+++ book/not.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+import Test.QuickCheck.All
+
+import Not1
+
+prop_myNot x   = not x == myNot x
+
+prop_myAnd x y = (x && y) == (x `myAnd` y)
+prop_myOr  x y = (x || y) == (x `myOr`  y)
+
+prop_mymin1 a b = a >= min' a b && b >= min' a b
+
+prop_mymin_three a b c = 
+  a >= minThree' a b c && 
+  b >= minThree' a b c && 
+  c >= minThree' a b c 
+
+prop_converToUpper1 = convertToUpper 'a' == 'A'
+prop_converToUpper2 = convertToUpper 'B' == 'B'
+prop_converToUpper3 = convertToUpper 'c' == 'C'
+prop_converToUpper4 = convertToUpper '?' == '?'
+
+main = do 
+  $(quickCheckAll)
---
+++ book/not1.hs
@@ -0,0 +1,33 @@
+module Not1 where
+
+myNot True  = False
+myNot False = True
+
+myAnd True True = True
+myAnd _ _ = False
+
+myOr True _ = True
+myOr _ True = True
+myOr _ _    = False
+
+threeDifferent :: Integer -> Integer -> Integer -> Bool
+threeDifferent x y z = x /= y && y /= z && x /= z
+
+-- 3.14
+min' :: Int -> Int -> Int
+min' a b  
+  | a < b = a
+  | otherwise = b
+
+minThree' :: Int -> Int -> Int -> Int
+minThree' a b c = min' a $ min' b c
+
+
+-- 3.16
+offset = fromEnum 'A' - fromEnum 'a'
+toUpper ch = toEnum (fromEnum ch + offset)
+isLowerChar ch = fromEnum ch >= fromEnum 'a' && fromEnum ch <= fromEnum 'z'
+
+convertToUpper :: Char -> Char
+convertToUpper c | isLowerChar c = toUpper c 
+                 | otherwise = c
