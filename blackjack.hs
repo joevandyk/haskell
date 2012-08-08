3f79a81e289b855bcb64424d64ba9c1db319ec12
Joe Van Dyk <joe@fixieconsulting.com>
2012-08-08 14:32:23 -0700

    wip

--- blackjack.hs
+++ blackjack.hs
@@ -13,25 +13,30 @@ instance Show Card where
 
 -- Cards should be compared by looking at the rank
 instance Ord Card where compare = comparing rank
---instance Eq Card where (==) (Card rank _) (Card rank1 _) = rank == rank1
+instance Eq Card where (==) (Card rank _) (Card rank1 _) = rank == rank1
 
-data Deck = Deck [Card] deriving (Show)
-data Hand = Hand [Card] deriving (Show)
+type Deck = [Card] -- Should these be an actual type?
+type Hand = [Card]
+
+-- TODO fix Aces
+scoreHand :: Hand -> Int
+scoreHand hand = sum ranks
+  where ranks = map (\c -> 2 + (fromEnum $ rank c)) hand
 
 -- Returns a new deck
 newDeck :: Deck
-newDeck = Deck [ Card r s | r <- [minBound..maxBound], s <- [minBound..maxBound] ]
+newDeck = [ Card r s | r <- [minBound..maxBound], s <- [minBound..maxBound] ]
 
 puts = putStrLn
 
 main = do
-  puts $ show $ Card Ace Hearts > Card Three Spades
-  puts $ show $ Card Ace Spades > Card Ace Hearts
-  puts $ show $ Card Two Hearts == Card Two Spades
-  puts $ show $ newDeck
-  puts "done"
-  
+  print $ scoreHand [Card Two Hearts, Card Ace Spades]
+  print newDeck
+  puts $ "There are " ++ (show $ length newDeck) ++ " cards in this deck."
+
+  return ()
   -- Tasks: 
   --   1. Be able to create and shuffle decks
   --   2. Be able to deal hands from a deck
   --   3. Given hands, be able to see which hand wins.
+  --   More
