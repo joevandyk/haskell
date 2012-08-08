947fbbba3651ea8eb574f0eab6d49239b149a05b
Joe Van Dyk <joe@fixieconsulting.com>
2012-08-08 15:14:28 -0700

    wip

--- blackjack.hs
+++ blackjack.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import Data.Ord (comparing)
+import Test.QuickCheck.All
 
 data Suit = Hearts | Spades | Clubs | Diamonds
   deriving (Show, Eq, Enum, Bounded)
@@ -18,25 +21,44 @@ instance Eq Card where (==) (Card rank _) (Card rank1 _) = rank == rank1
 type Deck = [Card] -- Should these be an actual type?
 type Hand = [Card]
 
--- TODO fix Aces
+-- TODO fix Aces and duplication
+-- Probably should use fold or something here.
 scoreHand :: Hand -> Int
-scoreHand hand = sum ranks
-  where ranks = map (\c -> 2 + (fromEnum $ rank c)) hand
+scoreHand hand = sum $ map (score) hand
+  where 
+    score card = case rank card of 
+                  Jack  -> 10
+                  Queen -> 10
+                  King  -> 10
+                  Ace   -> 11
+                  otherwise -> 2 + (fromEnum $ rank card)
+
+-- Should I be returning a Bool here? Or a fancier type?
+bustedHand :: Hand -> Bool
+bustedHand hand = scoreHand hand > 21
+
+blackJack :: Hand -> Bool
+blackJack hand = scoreHand hand == 21
 
 -- Returns a new deck
 newDeck :: Deck
 newDeck = [ Card r s | r <- [minBound..maxBound], s <- [minBound..maxBound] ]
 
-puts = putStrLn
+main = $(quickCheckAll)
 
-main = do
-  print $ scoreHand [Card Two Hearts, Card Ace Spades]
-  print newDeck
-  puts $ "There are " ++ (show $ length newDeck) ++ " cards in this deck."
+-- Tasks: 
+--   1. Be able to create and shuffle decks
+--   2. Be able to deal hands from a deck
+--   3. Given hands, be able to see which hand wins.
+--   More
+--
 
-  return ()
-  -- Tasks: 
-  --   1. Be able to create and shuffle decks
-  --   2. Be able to deal hands from a deck
-  --   3. Given hands, be able to see which hand wins.
-  --   More
+prop_deckLength = length newDeck == 52
+prop_bustedHand1 = bustedHand [Card Two Hearts] == False
+prop_bustedHand2 = bustedHand [Card Two Hearts, Card King Spades, Card King Spades] == True
+prop_scoreHand1 = scoreHand [Card Two Hearts] == 2
+prop_scoreHand2 = scoreHand [Card Ace Hearts] == 11
+prop_scoreHand3 = scoreHand [Card King Hearts] == 10
+prop_scoreHand4 = scoreHand [Card Jack Clubs] == 10
+prop_scoreHand5 = scoreHand [Card Queen Clubs, Card Three Hearts] == 13
+prop_blackjack = blackJack [Card Jack Clubs, Card Ace Hearts] == True
